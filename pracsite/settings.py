c19e4e67a00cf9f8669c69da697d65fc5257126b
kimminki <kimminki@example.com>
2026-02-23 19:08:05 +0900

    DATABASE_URL 파싱 방어 코드 추가

--- pracsite/settings.py
+++ pracsite/settings.py
@@ -66,10 +66,10 @@ WSGI_APPLICATION = 'pracsite.wsgi.application'
 
 
 # Database
-DATABASE_URL = os.environ.get('DATABASE_URL', '')
-if DATABASE_URL:
+DATABASE_URL = os.environ.get('DATABASE_URL', '').strip()
+if DATABASE_URL and not DATABASE_URL.startswith('://'):
     import dj_database_url
-    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 else:
     DATABASES = {
         'default': {
